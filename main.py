239666b93509e2b5fd4bd7e6dfeaba92aa28f554
ptejeswaravarma-ai <ptejeswaravarma@gmail.com>
2025-12-31 20:14:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ products_db = {
     3: {"id": 3, "name": "Keyboard", "price": 79.99, "stock": 30, "category": "Electronics"},
     4: {"id": 4, "name": "Monitor", "price": 299.99, "stock": 15, "category": "Electronics"},
     5: {"id": 6, "name": "Desk", "price": 199.99, "stock": 5, "category": "Furniture"},
+    6: {"id": 7, "name": "Desk1", "price": 199.99, "stock": 5, "category": "Furniture"},
 }
 
 # ============================================================================
